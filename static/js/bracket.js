bc21014a060b5a152d0e26a45cc54e3bfc745fe8
obnicholson <40842118+obnicholson@users.noreply.github.com>
2019-02-23 18:06:37 -0700

    bracket construction and js responsive buttons

--- static/js/bracket.js
+++ static/js/bracket.js
@@ -12,56 +12,153 @@ var matchupArray = [];
 var schoolArray = [];
 
 
+// Function to filter game data by round
+// ####################################################################################################
+function selectRound(round, division) {
+    return function(game) {
+        return game.round === round && game.bracket === division;
+    }
+}
+
+
+// Function to build bracket
+// ####################################################################################################
+function buildBracket(round, divisionInitial, bracket, roundGames) {
+    var roundDiv = bracket.select(`#round${round}_${divisionInitial}`)
+
+    var gameDivs = roundDiv.selectAll('btn-group-vertical')
+        .data(roundGames)
+        .enter()
+        .append('div')
+        .classed('game', true)
+        .classed('btn-group-vertical', true)
+        .attr('data-game', d=>d.game_id);
+    
+    gameDivs.append('button')
+        .attr('type', 'button')
+        .classed('team1', true)
+        .classed('btn btn-outline-secondary btn-sm btn-block', true)
+        .attr('data-team', '1')
+        .text(d=>d.team1)
+    
+    gameDivs.append('button')
+        .attr('type', 'button')
+        .classed('team2', true)
+        .classed('btn btn-outline-secondary btn-sm btn-block', true)
+        .attr('data-team', '2')
+        .text(d=>d.team2)
+}
+
+
+// Function to get game info for given game
+// ####################################################################################################
+function getGameInfo(game, bracket) {
+    var game_id = game.attr('data-game');
+    var team1 = game.select("button[data-team='1']").text();
+    var team2 = game.select("button[data-team='2']").text(); 
+
+    var thisMatchup = matchupArray.find(function(matchup){
+        return matchup.game_id === game_id && matchup.team1 === team1 && matchup.team2 === team2;
+    }); 
+
+    var thisGame = gameArray.find(function(game){
+        return game.game_id === game_id;
+    });
+
+    // var gameMatchupData = Object.assign(thisMatchup, thisGame);
+
+    var gameInfo = {};
+    gameInfo.game = game;
+    gameInfo.nextGame = thisGame.nextGame;
+    gameInfo.nextGamePos = thisGame.nextGamePos;
+    gameInfo.bracket = bracket;
+    gameInfo.team1 = team1;
+    gameInfo.team2 = team2;
+    gameInfo.team1WinProb = thisMatchup.team1WinProb;
+    gameInfo.team2WinProb = thisMatchup.team2WinProb;
+
+    return gameInfo;
+}
+
+
+// Function to predict winner of given game
+// ####################################################################################################
+function predictWinner(gameInfo) {
+    var game = gameInfo.game;
+    var bracket = gameInfo.bracket;
+
+    var nextGameID = gameInfo.nextGame;
+    var nextGamePos = gameInfo.nextGamePos;
+
+    var team1 = gameInfo.team1;
+    var team2 = gameInfo.team2;
+
+    var team1WinProb = gameInfo.team1WinProb;
+    var team2WinProb = gameInfo.team2WinProb;
+
+    if (team1WinProb > team2WinProb) {
+        game.select("button[data-team='1']")
+            .classed('active', true);
+
+        game.select("button[data-team='2']")
+            .classed('active', false);
+
+        nextGame(nextGameID, nextGamePos, team1, bracket);
+    }
+    else {
+        game.select("button[data-team='2']")
+            .classed('active', true);
+
+        game.select("button[data-team='1']")
+            .classed('active', false)
+
+        nextGame(nextGameID, nextGamePos, team2, bracket);
+    }
+}
+
+
+// Function to populate next game based on projected winner
+// ####################################################################################################
+function nextGame(nextGameID, nextGamePos, winningTeam, bracket) {
+    var nextGame = bracket.select(`.game[data-game='${nextGameID}']`)
+        .select(`button[data-team='${nextGamePos}']`)
+    
+    nextGame.text(winningTeam);
+}
+
+
 // Function to predict winner of current round and populate next round's teams
 // ####################################################################################################
 function populateBracketRound(round, bracket) {
     round.each(function(d, i) {
         var game = d3.select(this);
-        var game_id = game.attr('data-game');
-        var team1 = game.select("div[data-team='1']").select('p').text();
-        var team2 = game.select("div[data-team='2']").select('p').text();
-        
-        var thisMatchup = matchupArray.find(function(matchup){
-            return matchup.game_id === game_id && matchup.team1 === team1 && matchup.team2 === team2;
-        })
 
-        var thisGame = gameArray.find(function(game){
-            return game.game_id === game_id;
-        })
-
-        var gameMatchupData = Object.assign(thisMatchup, thisGame);
-        var nextGameID = gameMatchupData.nextGame;
-        var nextGamePos = gameMatchupData.nextGamePos;
-        
-        if (gameMatchupData.team1WinProb > gameMatchupData.team2WinProb) {
-            game.select("div[data-team='1']")
-                .select('p')
-                .html(`<strong>${team1}</strong>`);
-
-            bracket.select(`.game[data-game='${nextGameID}']`)
-                .select(`div[data-team='${nextGamePos}']`)
-                .select('p')
-                .text(team1);
-        }
-        else {
-            game.select("div[data-team='2']")
-                .select('p')
-                .html(`<strong>${team2}</strong>`);
-
-                bracket.select(`.game[data-game='${nextGameID}']`)
-                .select(`div[data-team='${nextGamePos}']`)
-                .select('p')
-                .text(team2);
-        }
+        predictWinner(getGameInfo(game, bracket));
     });  
 }
 
-// Function to filter game data by round
+
+// Function run predictions for multiple rounds
 // ####################################################################################################
-function selectRound(round) {
-    return function(game) {
-        return game.round === round;
+function runPredictions(startingRound, bracket) {
+    for (var i=startingRound; i<7; i++){
+        var round = bracket.selectAll(`.round${i}`).selectAll('.game')
+
+        populateBracketRound(round, bracket);
     }
+
+    var round6 = bracket.selectAll('.round6').selectAll('.game');
+
+    var champion = round6.select('.active').text()
+    var championSchoolInfo = schoolArray.find(function(school){
+        return school.name === champion;
+    });
+    
+    var championSchoolLogo = championSchoolInfo.logo
+
+    round6.select('img')
+        .attr('src', championSchoolLogo);
+
 }
 
 
@@ -85,16 +182,8 @@ d3.json(gamesURL).then(function(gameData){
         gameArray[key].team1 = value;
     });
 
-    Object.entries(gameData.seed1).forEach(function([key, value]){
-        gameArray[key].seed1 = value;
-    });
-
     Object.entries(gameData.team2).forEach(function([key, value]){
         gameArray[key].team2 = value;
-    });
-
-    Object.entries(gameData.seed2).forEach(function([key, value]){
-        gameArray[key].seed2 = value;
     });
 
     Object.entries(gameData.next_game).forEach(function([key, value]){
@@ -113,6 +202,10 @@ d3.json(gamesURL).then(function(gameData){
             matchupArray.push(matchupInfo);
         });
 
+        Object.entries(matchupData.seed_team1).forEach(function([key, value]){
+            matchupArray[key].seed1 = value;
+        });
+
         Object.entries(matchupData.team1).forEach(function([key, value]){
             matchupArray[key].team1 = value;
         });
@@ -125,9 +218,14 @@ d3.json(gamesURL).then(function(gameData){
             matchupArray[key].team2 = value;
         });
 
+        Object.entries(matchupData.seed_team2).forEach(function([key, value]){
+            matchupArray[key].seed2 = value;
+        });
+
         Object.entries(matchupData.team2_win_probability).forEach(function([key, value]){
             matchupArray[key].team2WinProb = value;
         });
+
 
 // Get school data
 // ####################################################################################################
@@ -141,57 +239,94 @@ d3.json(gamesURL).then(function(gameData){
                 schoolArray[key].logo = value;
             });
 
-            console.log(gameArray);
-            console.log(matchupArray);
-            console.log(schoolArray);
-
 
 // Create bracket
 // ####################################################################################################
-        var round1Games = gameArray.filter(selectRound('1'));
-        var round2Games = gameArray.filter(selectRound('2'));
-        var round3Games = gameArray.filter(selectRound('3'));
-        var round4Games = gameArray.filter(selectRound('4'));
-        var round5Games = gameArray.filter(selectRound('5'));
-        var round6Games = gameArray.filter(selectRound('6'));
+            var round1Games_S = gameArray.filter(selectRound('1', 'South'));
+            var round1Games_W = gameArray.filter(selectRound('1', 'West'));
+            var round1Games_MW = gameArray.filter(selectRound('1', 'Midwest'));
+            var round1Games_E = gameArray.filter(selectRound('1', 'East'));
 
-        var bracket = d3.select('#bracket');
+            var round2Games_S = gameArray.filter(selectRound('2', 'South'));
+            var round2Games_W = gameArray.filter(selectRound('2', 'West'));
+            var round2Games_MW = gameArray.filter(selectRound('2', 'Midwest'));
+            var round2Games_E = gameArray.filter(selectRound('2', 'East'));
 
-        var round1Div = bracket.append('div')
-            .attr('id', 'round1');
+            var round3Games_S = gameArray.filter(selectRound('3', 'South'));
+            var round3Games_W = gameArray.filter(selectRound('3', 'West'));
+            var round3Games_MW = gameArray.filter(selectRound('3', 'Midwest'));
+            var round3Games_E = gameArray.filter(selectRound('3', 'East'));
 
-        var round1GameDivs = round1Div.selectAll('div')
-            .data(round1Games)
-            .enter()
-            .append('div')
-            .classed('game', true)
-            .attr('data-game', d=>d.game_id);
-        
-        round1GameDivs.append('div')
-            .attr('data-team', '1')
-            .append('p')
-            .text(d=>d.team1)
-        
-        round1GameDivs.append('div')
-            .attr('data-team', '2')
-            .append('p')
-            .text(d=>d.team2)
+            var round4Games_S = gameArray.filter(selectRound('4', 'South'));
+            var round4Games_W = gameArray.filter(selectRound('4', 'West'));
+            var round4Games_MW = gameArray.filter(selectRound('4', 'Midwest'));
+            var round4Games_E = gameArray.filter(selectRound('4', 'East'));
+
+            var round5Games_S_W = gameArray.filter(selectRound('5', 'FF_W_S'));
+            var round5Games_E_MW = gameArray.filter(selectRound('5', 'FF_E_MW'));
+
+            var bracket = d3.select('#bracket');
+
+            buildBracket('1', 's',  bracket, round1Games_S);
+            buildBracket('1', 'w',  bracket, round1Games_W);
+            buildBracket('1', 'mw',  bracket, round1Games_MW);
+            buildBracket('1', 'e',  bracket, round1Games_E);
+
+            buildBracket('2', 's',  bracket, round2Games_S);
+            buildBracket('2', 'w',  bracket, round2Games_W);
+            buildBracket('2', 'mw',  bracket, round2Games_MW);
+            buildBracket('2', 'e',  bracket, round2Games_E);
+
+            buildBracket('3', 's',  bracket, round3Games_S);
+            buildBracket('3', 'w',  bracket, round3Games_W);
+            buildBracket('3', 'mw',  bracket, round3Games_MW);
+            buildBracket('3', 'e',  bracket, round3Games_E);
+
+            buildBracket('4', 's',  bracket, round4Games_S);
+            buildBracket('4', 'w',  bracket, round4Games_W);
+            buildBracket('4', 'mw',  bracket, round4Games_MW);
+            buildBracket('4', 'e',  bracket, round4Games_E);
+
+            buildBracket('5', 's_w',  bracket, round5Games_S_W);
+            buildBracket('5', 'e_mw',  bracket, round5Games_E_MW);
 
 
-// Populate bracket predictions
+// Populate initial bracket predictions; make responsive to recalculate predictions on user button selection
 // ####################################################################################################
-            var round1 = bracket.select('#round1').selectAll('.game')
-            var round2 = bracket.select('#round2').selectAll('.game')
-            var round3 = bracket.select('#round3').selectAll('.game')
-            var round4 = bracket.select('#round4').selectAll('.game')
-            var round5 = bracket.select('#round5').selectAll('.game')
-            var round6 = bracket.select('#round6').selectAll('.game')
+            runPredictions(1, bracket);
 
-            populateBracketRound(round1, bracket);
-            populateBracketRound(round2, bracket);
-            populateBracketRound(round3, bracket);
-            populateBracketRound(round4, bracket);
+            var changeWinnerButtons = d3.selectAll('.btn-group-vertical').selectAll('.btn');
+
+            changeWinnerButtons.on('click', function() {
+                var selectedButton = d3.select(this);
+                var selectedRound = selectedButton.data()[0].round;
+
+                var gameID = selectedButton.data()[0].game_id;
+                var game = bracket.select(`.game[data-game='${gameID}']`)
+
+                var gameInfo = getGameInfo(game, bracket);
+
+                var nextGameID = gameInfo.nextGame
+                var nextGamePos = gameInfo.nextGamePos;
+                var selectedWinnerName = selectedButton.text();
+
+                nextGame(nextGameID, nextGamePos, selectedWinnerName, bracket);
+
+                var nextRound = parseInt(selectedRound) + 1
+
+                runPredictions(nextRound, bracket);
+            });
         });
     });
 });
+
+
+// UPDATE BUTTON CLICK RESPONSE FOR FINALS SELECTION TO CHANGE HIGHLIGHT AND LOGO
+// RESET BUTTON
+// INDIVIDUAL DIVISIONS ZOOM IN???
+
+
+
+
+
 
